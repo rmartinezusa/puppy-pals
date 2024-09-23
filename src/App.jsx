a579cf0ff078f4b80fa5466f13bd3743eb6757dc
Rogelio Martinez <rogejr89@gmail.com>
2024-09-23 17:35:56 -0400

    Added key to fix warning error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       {
         puppies.map((puppy)=>{
-          return <p>{puppy.name}</p>
+          return <p key={puppy.id}>{puppy.name}</p>
         })
       }
     </div>
