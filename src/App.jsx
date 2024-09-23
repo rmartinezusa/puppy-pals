3b083939f40edf60b0565ce817b57a4b115a9e25
Rogelio Martinez <rogejr89@gmail.com>
2024-09-23 18:59:38 -0400

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import { puppyList } from './data.js'
 import { useState } from 'react'
+import './App.css'
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
+  const [featPupId, setFeatPupId] = useState(null);  
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
     <div className="App">
-      {
-        puppies.map((puppy)=>{
-          return <p key={puppy.id}>{puppy.name}</p>
-        })
+      <div className='puppyListMani'>
+        {
+          puppies.map((puppy)=>{
+            return <p onClick={() => {
+              setFeatPupId(puppy.id)
+            }} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
+
+      { 
+        featPupId && (
+          <div className="selectedPuppy">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+              </ul>
+              </div>
+        )
       }
     </div>
     </>
   );
 }
-  
-
 export default App
